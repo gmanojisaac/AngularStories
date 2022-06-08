ac8f3b868ddc527ff3e12cd7e600011d9148db6f
gmanojisaac <gmanoj.isaac@gmail.com>
2022-06-08 09:31:33 +0530

    Mermaid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
 
+	import { Component, OnInit, VERSION } from '@angular/core';
+	import mermaid from 'mermaid';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AngularStories';
+ngOnInit(): void {
+    mermaid.initialize({
+      securityLevel: 'loose'
+    });
+    mermaid.init();
+  }
+
 }
