ea27be6bbdc792f4d3b024245cd5cfa59ea916e5
gmanojisaac <gmanoj.isaac@gmail.com>
2022-06-08 11:31:36 +0530

    Markdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 
-	import { Component, OnInit, VERSION } from '@angular/core';
-	import mermaid from 'mermaid';
+import { Component, OnInit, VERSION } from '@angular/core';
+import mermaid from 'mermaid';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+	angularVersion = VERSION.full;
+	ngxMarkdownVersion = '12.0.1';
+	markdown = `## Markdown __rulez__!
+---
+### Syntax highlight
+\`\`\`typescript
+const language = 'typescript';
+\`\`\`
+### Lists
+1. Ordered list
+2. Another bullet point
+   - Unordered list
+   - Another unordered bullet
+### Blockquote
+> Blockquote to the max`;
+
   title = 'AngularStories';
 ngOnInit(): void {
     mermaid.initialize({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MarkdownModule } from 'ngx-markdown';
+
+import 'prismjs';
+import 'prismjs/components/prism-typescript.min.js';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
+import 'prismjs/plugins/line-highlight/prism-line-highlight.js';
+
+import 'prismjs/plugins/toolbar/prism-toolbar.min.js';
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard.min.js';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule, 
+    MarkdownModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
